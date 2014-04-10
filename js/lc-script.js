e1b5c5aa67de59020c8352f03a55b19c98e47324
Eddy <eaguilarsmind@gmail.com>
2014-04-10 11:10:41 -0400

    Initial commit Javascript

---
+++ js/lc-script.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function($){
+	$(document).on( 'submit', 'form[action*="'+wtm.form_action_url+'"]', function(e) {
+    
+    form = $(this);
+
+    if( form.attr('data-submit') == 1)
+    
+    return;
+    
+    e.preventDefault();    
+    
+    form.removeAttr('target');
+    
+    var email, fname, lname;
+
+    email = form.find('input[name*="email"]').val() || form.find('input[name*="EMAIL"]').val() ; 
+    
+    fname = form.find('input[name*="fname"]').val() || form.find('input[name*="FNAME"]').val() ; 
+    
+    lname = form.find('input[name*="lname"]').val() || form.find('input[name*="LNAME"]').val() ; 
+    
+    var form_data = {
+       
+       action : 'lc_ajax_mailchimp_subscribe',
+       
+       sub_email :  email,
+       
+       first_name:  fname,
+       
+       last_name:  lname,
+     }
+    
+     $.ajax({
+    
+      url: wtm.ajaxUrl,
+   
+      data: form_data,
+
+      type: 'POST',
+
+      dataType : 'json',
+
+      success : function(resp){
+       
+         form.attr('data-submit', 1);
+        
+         form.submit();    
+              
+       }
+
+      
+
+
+     })
+     
+     
+
+  })
+  
+  
+
+})
